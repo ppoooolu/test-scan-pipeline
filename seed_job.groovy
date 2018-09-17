e1158f3665d4112d80dc7b107807d51ec151ce33
Constantine Jin <constantine.jin@blackboard.com>
2018-09-17 17:00:17 +0800

    add

--- seed_job.groovy
+++ seed_job.groovy
@@ -3,12 +3,15 @@ pipeline {
     agent any
     stages{
         stage('Generate jobs'){
-            jobDsl(
-                    additionalClasspath: 'src',
-                    removedJobAction: 'DELETE',
-                    removedViewAction: 'DELETE',
-                    targets: 'dls/*',
-                    unstableOnDeprecation: true)
+            steps {
+                jobDsl(
+                        additionalClasspath: 'src',
+                        removedJobAction: 'DELETE',
+                        removedViewAction: 'DELETE',
+                        targets: 'dls/*',
+                        unstableOnDeprecation: true
+                )
+            }
         }
     }
 }
