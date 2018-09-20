e40b3af62adff6c0eae852e7548b3f50252229ef
Constantine Jin <constantine.jin@blackboard.com>
2018-09-20 14:51:15 +0800

    add

--- src/main/groovy/io/unguiculus/jobdsl/CreateMulitibranchPipelineJob.groovy
+++ src/main/groovy/io/unguiculus/jobdsl/CreateMulitibranchPipelineJob.groovy
@@ -46,6 +46,14 @@ class CreateMulitibranchPipelineJob {
 //                        }
 //                    }
                 }
+
+                strategy {
+                    defaultBranchPropertyStrategy {
+                        props {
+                            noTriggerBranchProperty()
+                        }
+                    }
+                }
             }
 
 //            factory {
