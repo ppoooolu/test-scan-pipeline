5ca43e31d6e784a600537ac8e3ddc2863f8cc0bc
Constantine Jin <constantine.jin@blackboard.com>
2018-09-20 14:06:45 +0800

    add

--- dsl_jobs/test_create_dsl_job.groovy
+++ dsl_jobs/test_create_dsl_job.groovy
@@ -1,5 +1,5 @@
 import javaposse.jobdsl.dsl.Job
 //import static org.Fleets
-import learnsaas.dsl.CreateMulitibranchPipelineJob
+import dsl.CreateMulitibranchPipelineJob
 
 Job collectParamsJob = new CreateMulitibranchPipelineJob().build(this)
